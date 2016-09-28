584e4972e029bb7ee1c3b41f71e79e68cb8408c5
RuaSamarah <ruash.samarah@gmail.com>
2016-09-28 15:31:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ruaasamara/gridview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ruaasamara.gridview;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.GridView;
+import android.widget.GridLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+
+        GridView gridView = new GridView(this);
+        gridView.setVerticalSpacing(2);
+        gridView.setHorizontalSpacing(2);
+        gridView.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
+        gridView.setNumColumns(3);
+        gridView.setGravity(Gravity.CENTER);
+        gridView.setBackgroundColor(Color.BLACK);
+        gridView.setLayoutParams(new GridView.LayoutParams(GridLayout.LayoutParams.MATCH_PARENT,GridLayout.LayoutParams.MATCH_PARENT));
+        gridView.setAdapter(new ImageAdapter(this));
+
+        setContentView(gridView);
+
+
+    }
+}
